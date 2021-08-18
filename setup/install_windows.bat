501277c69ce6a779ad2d3b77351a618eb60a955b
James Bartolozzi <bartolozzij@gmail.com>
2021-08-18 15:16:21 -0400

    added otls
    added

---
+++ setup/install_windows.bat
@@ -0,0 +1,2 @@
+mklink /D "C:\Users\james\Documents\houdini18.5\python2.7libs\StudioXtras" "%~dp0..\StudioXtras\python2.7libs\StudioXtras"
+mklink /D "C:\Users\james\Documents\houdini18.5\otls\StudioXtras" "%~dp0..\StudioXtras\otls"
