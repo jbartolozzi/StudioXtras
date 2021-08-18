6fe5da8bae4fddef7e85e8f58725123e2a6a2f4e
James Bartolozzi <bartolozzij@gmail.com>
2021-08-18 16:29:18 -0400

    Update install_windows.bat

--- setup/install_windows.bat
+++ setup/install_windows.bat
@@ -1,1 +1,1 @@
-mklink /D "C:\Users\james\Documents\houdini18.5\python2.7libs\StudioXtras" "%~dp0..\StudioXtras\python2.7libs\StudioXtras"
+mklink /D "C:\%username%\james\Documents\houdini18.5\python2.7libs\StudioXtras" "%~dp0..\StudioXtras\python2.7libs\StudioXtras"
