faa5b8877b39b1018e7a4b940308bf5f42812608
James Bartolozzi <bartolozzij@gmail.com>
2021-08-18 14:52:48 -0400

    adding files
    init

---
+++ StudioXtras/python2.7libs/StudioXtras/ffmpeg.py
@@ -0,0 +1,99 @@
+import hou
+import os
+import sys
+import threading
+
+from StudioXtras import utils
+
+def _createImageList(first_frame, last_frame, step, picture_parm, enable_denoise_postfix, denoise_postfix):
+    def getExt(input):
+        return "." + input.split(".")[-1]
+    output = []
+    threads = []
+    files_to_delete = []
+    for frame in range(int(first_frame), int(last_frame + 1), int(step)):
+        frame_file = picture_parm.evalAtFrame(frame)
+        
+        if (enable_denoise_postfix):
+            ext = getExt(frame_file)
+            frame_file = frame_file.replace(ext, denoise_postfix + ext)
+        
+        if not frame_file.endswith(".jpg"):
+            # Create a thread to run iconvert
+            jpg_frame_file = frame_file.replace(getExt(frame_file), ".jpg")
+            
+            command = "iconvert -g auto %s %s" % (frame_file, jpg_frame_file)
+            t = threading.Thread(target=utils.runCommand, args=(command,))
+            threads.append(t)
+            t.start()
+
+            files_to_delete.append(jpg_frame_file)
+            frame_file = jpg_frame_file
+
+        frame_file = os.path.basename(frame_file)
+        output.append("file \'%s\'" % frame_file)
+    for thread in threads:
+        thread.join()
+    return (output, files_to_delete)
+
+
+def _writeFfmpegList(output_file, output_list):
+    with open(output_file, 'w') as f:
+        for item in output_list:
+            f.write("%s\n" % item)
+
+
+def run():
+    # Boiler plate setup
+    utils.makeTimestampEnv()
+    node = hou.pwd()
+    helper = utils.RopHelper("Slack")
+    
+    # Find ffmpeg executable
+    ffmpeg_executable = helper.executablePath("ffmpeg")
+   
+    # Get the target ROP for source images
+    op = helper.getTargetOutputNode(node)
+
+    # Create the list of images to pass to FFMPEG
+    f1 = helper.getParm(op, "f1")
+    f2 = helper.getParm(op, "f2")
+    f3 = helper.getParm(op, "f3")
+    fps = helper.getParm(node, "fps")
+    fps = max(fps / f3, 1)
+
+    picture_parm = helper.getPictureParm(op)
+
+    # Create image list
+    list_file = os.path.join(os.path.dirname(picture_parm.eval()), "ffmpeg_list.txt")
+
+    # Write image list to render directory
+    enable_denoise_postfix = helper.getParm(node, "enable_denoise_postfix")
+    denoise_postfix = helper.getParm(node, "denoise_postfix")
+
+    image_list, files_to_delete = _createImageList(f1, f2, f3, picture_parm, enable_denoise_postfix, denoise_postfix)
+
+    _writeFfmpegList(list_file, image_list)
+
+    # Prepare directory for output file
+    output_file = node.parm("vm_picture").eval()
+    output_directory = os.path.dirname(output_file)
+
+    output_file = output_file.replace(" ", "\\ ")
+
+    if not os.path.exists(output_directory):
+        os.mkdir(output_directory)
+
+    command = "\"%s\" -y -r %s -f concat -i \"%s\" -r %s %s \"%s\"" % (ffmpeg_executable, fps,
+                                                           list_file, fps, node.parm("advanced_parameters").evalAsString(), output_file)
+
+    cmd_output, cmd_err = utils.runCommand(command)
+
+    files_to_delete.append(list_file)
+    for del_file in files_to_delete:
+        if os.path.exists(del_file):
+            os.remove(del_file)
+    
+    if cmd_err != "" and "error" in cmd_err.lower():
+        logger.error(cmd_err)
+
---
+++ StudioXtras/python2.7libs/StudioXtras/utils.py
@@ -0,0 +1,69 @@
+import hou
+import sys
+import subprocess
+from distutils.spawn import find_executable
+
+class NodeHelper:
+    def __init__(self, node_name):
+        self.node_name = node_name
+
+    def error(self, text):
+        err_text = "ERROR! StudioXtras:%s:\n%s" % (self.node_name, str(text))
+        sys.stderr.write(err_text + "\n")
+        if "ui" in dir(hou):    
+            raise hou.NodeError(err_text)
+
+    def log(self, text):
+        print("StudioXtras:%s: %s" % (self.node_name, text))
+
+    def getParm(self, node, parm_name):
+        try:
+            return node.parm(parm_name).eval()
+        except:
+            self.error("Unable to get parm %s for node %s." % (parm_name, node.name()))
+            return None
+
+    def executablePath(self, executable_name):
+        executable_path = find_executable(executable_name)
+        if executable_path is None:
+            self.error("%s is not installed. Please install %s and update your PATH environment variable." % (executable_name, executable_name)) 
+        return executable_path
+
+class RopHelper(NodeHelper):
+    def getTargetOutputNode(self, node):
+        if len(node.inputs()) > 0:
+            op = node.inputs()[0]
+        else:
+            op = hou.node(self.getParm(node, "output_driver"))
+
+        # Check if op is a fetch, resolve the fetch path as new op
+        if op is not None and op.type().name() == "fetch":
+            op = hou.node(op.parm("source").eval())
+
+        if op is None:
+            self.error("Output ROP does not exist.")
+
+        return op
+
+    def getPictureParm(self, node):
+        parm_names = ["ar_picture", "picture", "vm_picture", "ri_display_0", "copoutput", "HO_img_fileName"]
+        for parm_name in parm_names:
+            if node.parm(parm_name) is not None:
+                return node.parm(parm_name)
+        
+        if picture_parm is None:
+            self.error("Unable to find picture or vm_picture parm from render target.")
+        return picture_parm
+
+def runCommand(command, disable_env=False):
+    if disable_env:
+        process = subprocess.Popen(command, env={}, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    else:
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+    cmd_output, cmd_err = process.communicate()
+
+    return (cmd_output, cmd_err)
+
+def makeTimestampEnv():
+    hou.putenv("TIMESTAMP", hou.expandString("$_HIP_SAVETIME").strip().replace(" ", "_").replace(":", "_"))
