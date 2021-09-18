138901ad2d895d039e3c26b5adc520dd0b3cce20
James Bartolozzi <bartolozzij@gmail.com>
2021-09-18 14:57:44 -0700

    added denoiser preset

--- python2.7libs/StudioXtras/Renderbot.py
+++ python2.7libs/StudioXtras/Renderbot.py
@@ -1,5 +1,6 @@
 import hou
-SETTINGS_LOW = {"ar_AA_samples": 1,
+SETTINGS_LOW = \
+    {"render": {"ar_AA_samples": 1,
                 "ar_enable_adaptive_sampling": True,
                 "ar_AA_samples_max": 2,
                 "ar_AA_adaptive_threshold": 0.05,
@@ -8,9 +9,13 @@ SETTINGS_LOW = {"ar_AA_samples": 1,
                 "ar_GI_transmission_samples": 1,
                 "ar_GI_sss_samples": 1,
                 "ar_GI_volume_samples": 1,
-                }
+                },
+     "denoiser": {
+        "variance": 0.8,
+    }}
 
-SETTINGS_MED = {"ar_AA_samples": 2,
+SETTINGS_MED = \
+    {"render": {"ar_AA_samples": 2,
                 "ar_enable_adaptive_sampling": True,
                 "ar_AA_samples_max": 4,
                 "ar_AA_adaptive_threshold": 0.025,
@@ -19,17 +24,24 @@ SETTINGS_MED = {"ar_AA_samples": 2,
                 "ar_GI_transmission_samples": 2,
                 "ar_GI_sss_samples": 2,
                 "ar_GI_volume_samples": 2,
-                }
-SETTINGS_HIGH = {"ar_AA_samples": 3,
-                 "ar_enable_adaptive_sampling": True,
-                 "ar_AA_samples_max": 8,
-                 "ar_AA_adaptive_threshold": 0.01,
-                 "ar_GI_diffuse_samples": 2,
-                 "ar_GI_specular_samples": 2,
-                 "ar_GI_transmission_samples": 2,
-                 "ar_GI_sss_samples": 2,
-                 "ar_GI_volume_samples": 2,
-                 }
+                },
+     "denoiser": {
+        "variance": 0.4,
+    }}
+SETTINGS_HIGH = \
+    {"render": {"ar_AA_samples": 3,
+                "ar_enable_adaptive_sampling": True,
+                "ar_AA_samples_max": 8,
+                "ar_AA_adaptive_threshold": 0.01,
+                "ar_GI_diffuse_samples": 2,
+                "ar_GI_specular_samples": 2,
+                "ar_GI_transmission_samples": 2,
+                "ar_GI_sss_samples": 2,
+                "ar_GI_volume_samples": 2,
+                },
+     "denoiser": {
+        "variance": 0.25,
+    }}
 
 
 def updateArnoldPreset():
@@ -39,11 +51,27 @@ def updateArnoldPreset():
 
     mode = hou.pwd().parm("preset").eval()
     if mode == 0:
-        for parm_name, val in SETTINGS_LOW.items():
+        for parm_name, val in SETTINGS_LOW["render"].items():
             arnold_rop.parm(parm_name).set(val)
     elif mode == 1:
-        for parm_name, val in SETTINGS_MED.items():
+        for parm_name, val in SETTINGS_MED["render"].items():
             arnold_rop.parm(parm_name).set(val)
     elif mode == 2:
-        for parm_name, val in SETTINGS_HIGH.items():
+        for parm_name, val in SETTINGS_HIGH["render"].items():
             arnold_rop.parm(parm_name).set(val)
+
+    if hou.pwd().parm("enable_denoiser"):
+        denoiser_rop = hou.node(hou.pwd().path() + "/arnold_denoiser1")
+        if denoiser_rop is None:
+            raise hou.NodeWarning("Cannot locate arnold ROP")
+
+        mode = hou.pwd().parm("preset").eval()
+        if mode == 0:
+            for parm_name, val in SETTINGS_LOW["denoiser"].items():
+                denoiser_rop.parm(parm_name).set(val)
+        elif mode == 1:
+            for parm_name, val in SETTINGS_MED["denoiser"].items():
+                denoiser_rop.parm(parm_name).set(val)
+        elif mode == 2:
+            for parm_name, val in SETTINGS_HIGH["denoiser"].items():
+                denoiser_rop.parm(parm_name).set(val)
