ab5e44a2faea1d42b92002a33b03caa482bb0907
James Bartolozzi <bartolozzij@gmail.com>
2021-11-14 22:20:48 -0800

    fixing up presets for ffmpeg

--- python2.7libs/StudioXtras/Ffmpeg.py
+++ python2.7libs/StudioXtras/Ffmpeg.py
@@ -1,5 +1,6 @@
 import hou
 import os
+import json
 import time
 
 from StudioXtras import Utils
@@ -22,6 +23,92 @@ def _writeFfmpegList(output_file, output_list):
     with open(output_file, 'w') as f:
         for item in output_list:
             f.write("%s\n" % item)
+
+
+def _CreateFFmpegCommand(ffmpeg_executable, configfile, codec, fps, list_file, output_file):
+    """
+    Adapted from https://github.com/jedypod/generate-dailies/blob/master/dailies-config.yaml
+    """
+
+    config = configfile
+    globals_config = config.get("globals")
+    codec_config = config["output_codecs"][codec]
+
+    # ffmpeg-10bit No longer necessary in ffmpeg > 4.1
+    ffmpeg_command = ffmpeg_executable
+
+    # if codec_config['bitdepth'] >= 10:
+    #     pixel_format = "rgb48le"
+    # else:
+    #     pixel_format = "rgb24"
+
+    if codec_config['name'] == 'mjpeg':
+        # Set up input arguments for frame input through pipe:
+        args = "{0} -y -r {1} -f concat -i {2}".format(
+            ffmpeg_command, fps, list_file)
+    else:
+        # Set up input arguments for raw video and pipe:
+        args = "{0} -hide_banner -loglevel info -y -f concat -r {1} -i {2}".format(
+            ffmpeg_command, fps, list_file)
+
+    # Add timecode so that start frame will display correctly in RV etc
+    # args += " -timecode {0}".format(start_tc)
+
+    if codec_config['codec']:
+        args += " -c:v {0}".format(codec_config['codec'])
+
+    if codec_config['profile']:
+        args += " -profile:v {0}".format(codec_config['profile'])
+
+    if codec_config['qscale']:
+        args += " -qscale:v {0}".format(codec_config['qscale'])
+
+    if codec_config['preset']:
+        args += " -preset {0}".format(codec_config['preset'])
+
+    if codec_config['keyint']:
+        args += " -g {0}".format(codec_config['keyint'])
+
+    if codec_config['bframes']:
+        args += " -bf {0}".format(codec_config['bframes'])
+
+    if codec_config['tune']:
+        args += " -tune {0}".format(codec_config['tune'])
+
+    if codec_config['crf']:
+        args += " -crf {0}".format(codec_config['crf'])
+
+    if codec_config['pix_fmt']:
+        args += " -pix_fmt {0}".format(codec_config['pix_fmt'])
+
+    if codec_config['vf']:
+        args += " -vf {0}".format(codec_config['vf'])
+
+    if codec_config['vendor']:
+        args += " -vendor {0}".format(codec_config['vendor'])
+
+    if codec_config['metadata_s']:
+        args += " -metadata:s {0}".format(codec_config['metadata_s'])
+
+    if codec_config['bitrate']:
+        args += " -b:v {0}".format(codec_config['bitrate'])
+
+    if codec_config['movie_ext']:
+        output_file = os.path.splitext(output_file)[0] + "." + codec_config['movie_ext']
+
+    # Finally add the output movie file path
+    args += " -r {0} {1}".format(fps, output_file)
+    return args
+
+
+def getConfigList(configjson):
+    codecs = ["custom"]
+    codecs.extend(sorted(configjson["output_codecs"].keys()))
+    output = []
+    for codec in codecs:
+        output.append(codec)
+        output.append(codec.title())
+    return output
 
 
 def run():
@@ -47,10 +134,13 @@ def run():
     fps = helper.getParm(node, "fps")
     fps = max(fps / f3, 1)
 
+    gamma = node.parm("gamma").eval()
+
     helper.debug("f1: %s" % f1)
     helper.debug("f2: %s" % f2)
     helper.debug("f3: %s" % f3)
     helper.debug("fps: %s" % fps)
+    helper.debug("gamma: %s" % gamma)
 
     picture_parm = helper.getPictureParm(op)
     dirname = os.path.dirname(picture_parm.eval())
@@ -67,16 +157,21 @@ def run():
     list_file = os.path.join(os.path.dirname(picture_parm.eval()), "ffmpeg_list.txt")
     helper.debug("list_file: %s" % list_file)
 
-    # The COP output is the converted JPG we use for the video
-    cop_output = hou.node("./convert/output")
-    output_image_parm = cop_output.parm("copoutput")
+    if node.parm("ocio_convert").eval():
+        # The COP output is the converted JPG we use for the video
+        cop_output = hou.node("./convert/output")
+        output_image_parm = cop_output.parm("copoutput")
 
-    # Create the ffmpeg imagelist, and list of temp files to delete
-    image_list, files_to_delete = _createImageList(
-        f1, f2, f3, output_image_parm)
+        # Create the ffmpeg imagelist, and list of temp files to delete
+        image_list, files_to_delete = _createImageList(
+            f1, f2, f3, output_image_parm)
 
-    # Render out the temp converted images
-    cop_output.render()
+        # Render out the temp converted images
+        cop_output.render()
+
+    else:
+        image_list, files_to_delete = _createImageList(
+            f1, f2, f3, picture_parm)
 
     # Write the ffmpeg imagelist
     _writeFfmpegList(list_file, image_list)
@@ -91,12 +186,33 @@ def run():
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
-    # Run ffmpeg
-    command = "\"%s\" -y -r %s -f concat -i \"%s\" -r %s %s \"%s\"" % (ffmpeg_executable, fps,
-                                                                       list_file, fps, node.parm("advanced_parameters").evalAsString(), output_file)
+    configjson = json.loads(hou.pwd().type().definition().sections()
+                            ['dailies-config.json'].contents())
+    config_list = getConfigList(configjson)
+    preset = config_list[hou.pwd().parm("ffmpeg_preset").eval() * 2]
 
-    cmd_output, cmd_err = Utils.runCommand(command)
-    helper.debug(command)
+    if preset != "custom":
+        ffmpeg_command = _CreateFFmpegCommand(
+            ffmpeg_executable,
+            configjson,
+            preset,
+            fps,
+            list_file,
+            output_file)
+
+    else:
+        # Run ffmpeg
+        ffmpeg_command = "\"%s\" -hide_banner -y -r %s -gamma %s -f concat -i \"%s\" -r %s %s \"%s\"" % \
+            (ffmpeg_executable,
+                fps,
+                gamma,
+                list_file,
+                fps,
+                node.parm("advanced_parameters").evalAsString(),
+                output_file)
+
+    cmd_output, cmd_err = Utils.runCommand(ffmpeg_command)
+    helper.debug(ffmpeg_command)
     helper.debug("%s\n%s" % (cmd_output, cmd_err))
 
     files_to_delete.append(list_file)
