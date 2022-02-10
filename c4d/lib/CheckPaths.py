bd27e0a30e30afa7d6195f213cd0e7272f69678a
bartolozzij2 <96601070+bartolozzij2@users.noreply.github.com>
2022-02-10 16:14:36 -0500

    major updates and fixes

--- c4d/lib/CheckPaths.py
+++ c4d/lib/CheckPaths.py
@@ -14,6 +14,9 @@ ARNOLD_SHADER_NETWORK = 1033991
 ARNOLD_SHADER_GV = 1033990
 ARNOLD_C4D_SHADER_GV = 1034190
 
+# Cinema Codes
+CINEMA_SHADER_NETWORK = 5703
+
 # from api/util/NodeIds.h
 C4DAIN_IMAGE = 262700200
 
@@ -32,75 +35,98 @@ ARNOLD_PROCEDURAL_USD_FILE = 617912960
 ARNOLD_PROCEDURAL_ASS_FILE = c4d.C4DAI_PROCEDURAL_PATH
 
 
-class ArnoldTexture:
-    @staticmethod
-    def queryMaterialNetwork(material):
-        msg = c4d.BaseContainer()
-        msg.SetInt32(C4DTOA_MSG_TYPE, C4DTOA_MSG_QUERY_SHADER_NETWORK)
-        material.Message(c4d.MSG_BASECONTAINER, msg)
-        return msg
+class BaseAsset:
+    def __init__(self, c4d_object):
+        self.c4d_object = c4d_object
 
-    @staticmethod
-    def getPaths(doc):
-        '''
-        Return tuple of (object, file_path)
-        '''
-        output = []
-        for mat in doc.GetMaterials():
-            if mat.GetType() == ARNOLD_SHADER_NETWORK:
-                network = ArnoldTexture.queryMaterialNetwork(mat)
-                numShaders = network.GetInt32(C4DTOA_MSG_RESP1)
-                for i in range(0, numShaders):
-                    shader = network.GetLink(10000 + i)
-                    texture_path = ArnoldTexture.getTexturePath(shader)
-                    if texture_path:
-                        output.append((shader, texture_path))
-        return output
+    def getFilePath(self):
+        return None
 
-    @staticmethod
-    def getTexturePath(shader):
-        if shader is None:
+    def setFilePath(self, new_path):
+        pass
+
+    def getName(self):
+        try:
+            return self.c4d_object.GetName()
+        except:
+            return "Unnamed"
+
+    def __bool__(self):
+        return self.c4d_object is not None
+
+class ArnoldTexture(BaseAsset):
+
+    def getFilePath(self):
+        if self.c4d_object is None:
             return None
 
-        data = shader.GetOpContainerInstance()
+        data = self.c4d_object.GetOpContainerInstance()
 
-        if shader.GetOperatorID() == ARNOLD_SHADER_GV:
+        if self.c4d_object.GetOperatorID() == ARNOLD_SHADER_GV:
             nodeId = data.GetInt32(C4DAI_GVSHADER_TYPE)
             if nodeId == C4DAIN_IMAGE:
                 return data.GetFilename(C4DAIP_IMAGE_FILENAME)
 
-        if shader.GetOperatorID() == ARNOLD_C4D_SHADER_GV:
+        if self.c4d_object.GetOperatorID() == ARNOLD_C4D_SHADER_GV:
             nodeId = data.GetInt32(C4DAI_GVC4DSHADER_TYPE)
             if nodeId == c4d.Xbitmap:
                 # the C4D shader is attached to the GV node
-                c4d_shader = shader.GetFirstShader()
+                c4d_shader = self.c4d_object.GetFirstShader()
                 if c4d_shader is not None:
                     return c4d_shader.GetDataInstance().GetFilename(c4d.BITMAPSHADER_FILENAME)
 
         return None
 
-    @staticmethod
-    def setShaderUpdatedPath(shader, texture_path):
+    def setFilePath(self, new_path):
         MATERIAL_COMMAND_IMAGE = 1737748425
-        shader[MATERIAL_COMMAND_IMAGE] = texture_path
+        self.c4d_object[MATERIAL_COMMAND_IMAGE] = new_path
         c4d.EventAdd()
 
+    def __bool__(self):
+        return bool(self.c4d_object is not None and self.getFilePath() is not None)
 
-class ArnoldProcedural:
-    @staticmethod
-    def getPaths(doc):
-        output = []
-        arnold_procedurals = list(obj for obj in doc.GetObjects() if obj.GetType() == 1032509)
-        for arnold_procedural in arnold_procedurals:
-            for parm_id in [ARNOLD_PROCEDURAL_ASS_FILE, ARNOLD_PROCEDURAL_USD_FILE, ARNOLD_PROCEDURAL_ALEMBIC_FILE]:
-                if arnold_procedural[parm_id]:
-                    output.append((arnold_procedural, parm_id))
-        return output
 
-    @staticmethod
-    def setPath(arnold_procedural, parm_id, texture_path):
-        arnold_procedural[parm_id] = texture_path
+class CinemaTexture(BaseAsset):
+    def getFilePath(self):
+        return self.c4d_object[c4d.BITMAPSHADER_FILENAME] 
+
+    def setFilePath(self, new_path):
+        self.c4d_object[c4d.BITMAPSHADER_FILENAME] = new_path
         c4d.EventAdd()
+
+    def __bool__(self):
+        return bool(self.c4d_object is not None and self.c4d_object[c4d.BITMAPSHADER_FILENAME] is not None)
+
+class ArnoldProcedural(BaseAsset):
+    def __init__(self, c4d_object, parm_id):
+        self.c4d_object = c4d_object
+        self.parm_id = parm_id
+
+    def getFilePath(self):
+        return self.c4d_object[self.parm_id]
+
+    def setFilePath(self, new_path):
+        self.c4d_object[self.parm_id ] = new_path
+        c4d.EventAdd()
+
+    def __bool__(self):
+        return bool(self.c4d_object is not None and self.c4d_object[self.parm_id] is not None and self.c4d_object[self.parm_id] != "")
+
+
+class CinemaProcedural(BaseAsset):
+    def __init__(self, c4d_object):
+        self.c4d_object = c4d_object
+
+    def getFilePath(self):
+        return self.c4d_object[c4d.ALEMBIC_PATH]
+
+    def setFilePath(self, new_path):
+        self.c4d_object[c4d.ALEMBIC_PATH] = new_path
+        c4d.EventAdd()
+
+    def __bool__(self):
+        return bool(self.c4d_object is not None and self.c4d_object[c4d.ALEMBIC_PATH] is not None and self.c4d_object[c4d.ALEMBIC_PATH] != "")
+
 
 
 class PathMapper:
@@ -143,64 +169,112 @@ class PathMapper:
 
         return texture_path
 
-
-def updateImageTexturePaths(image_textures_to_check, pathmapper):
+def updateFilePaths(assets_to_check, pathmapper):
     num_missing = 0
     num_updated = 0
-    for shader, texture_path in image_textures_to_check:
-        updated = pathmapper.updatePath(texture_path)
+    for asset in assets_to_check:
+        updated = pathmapper.updatePath(asset.getFilePath())
         if not os.path.exists(updated):
             num_missing += 1
-            print("CheckPaths Cant find: %s" % updated)
-        elif updated != texture_path:
-            ArnoldTexture.setShaderUpdatedPath(shader, updated)
+            print("CheckPaths Updated Path \"%s\" doesnt exist for node \"%s\"." % (updated, asset.getName()))
+        elif updated != asset.getFilePath():
+            asset.setFilePath(updated)
             print("CheckPaths Updated: %s" % updated)
             num_updated += 1
     return (num_updated, num_missing)
 
 
-def updateProceduralPaths(procedurals_to_check, pathmapper):
-    num_missing = 0
-    num_updated = 0
-    for (arnold_procedural, parm_id) in procedurals_to_check:
-        file_path = arnold_procedural[parm_id]
-        updated = pathmapper.updatePath(file_path)
-        if not os.path.exists(updated):
-            num_missing += 1
-            print("CheckPaths Cant find: %s" % updated)
-        elif updated != file_path:
-            ArnoldProcedural.setPath(arnold_procedural, parm_id, updated)
-            print("CheckPaths Updated: %s" % updated)
-            num_updated += 1
-    return (num_updated, num_missing)
+def getAllCinemaTextures(doc):
+    output = []
+    for mat in list(mat for mat in doc.GetMaterials() if mat.GetType() == CINEMA_SHADER_NETWORK):
+        for mid in [c4d.MATERIAL_COLOR_SHADER, c4d.MATERIAL_ALPHA_SHADER, c4d.MATERIAL_DIFFUSION_SHADER, c4d.MATERIAL_LUMINANCE_SHADER, c4d.MATERIAL_NORMAL_SHADER, c4d.MATERIAL_TRANSPARENCY_SHADER, c4d.MATERIAL_ENVIRONMENT_SHADER, c4d.MATERIAL_BUMP_SHADER,]:
+            new_object = CinemaTexture(mat[mid])
+            if new_object:
+                output.append(new_object)
+
+        if mat[c4d.MATERIAL_USE_REFLECTION]:
+            for layernum in range(mat.GetReflectionLayerCount()):
+                layer = mat.GetReflectionLayerIndex(layernum)
+                new_object = CinemaTexture(mat[layer.GetDataID() + c4d.REFLECTION_LAYER_COLOR_TEXTURE])
+                if new_object:
+                    output.append(new_object)
+
+    return output
+
+def getAllArnoldTextures(doc):
+    def queryMaterialNetwork(material):
+        msg = c4d.BaseContainer()
+        msg.SetInt32(C4DTOA_MSG_TYPE, C4DTOA_MSG_QUERY_SHADER_NETWORK)
+        material.Message(c4d.MSG_BASECONTAINER, msg)
+        return msg
+
+    output = []
+    for mat in doc.GetMaterials():
+        if mat.GetType() == ARNOLD_SHADER_NETWORK:
+            network = queryMaterialNetwork(mat)
+            numShaders = network.GetInt32(C4DTOA_MSG_RESP1)
+            for i in range(0, numShaders):
+                shader = network.GetLink(10000 + i)
+                new_object = ArnoldTexture(network.GetLink(10000 + i))
+                if new_object:
+                    output.append(new_object)
+    return output
+
+
+def getAllArnoldProcedurals(doc, all_objects):
+    output = []
+    arnold_procedurals = list(obj for obj in all_objects if obj.GetTypeName() == "Arnold Procedural")
+    for procedural in arnold_procedurals:
+        for parm_id in [ARNOLD_PROCEDURAL_ASS_FILE, ARNOLD_PROCEDURAL_USD_FILE, ARNOLD_PROCEDURAL_ALEMBIC_FILE]:
+            new_object = ArnoldProcedural(procedural, parm_id)
+            if new_object:
+                output.append(new_object)
+    return output
+
+
+def getAllCinemaProcedurals(doc, all_objects):
+    output = []
+    procedurals = list(obj for obj in all_objects if obj.GetTypeName() == "Alembic Generator")
+    for procedural in procedurals:
+        new_object = CinemaProcedural(procedural)
+        if new_object:
+            output.append(new_object)
+    return output
+
+
+def getAllObjects(doc):
+    def recurse_hierarchy(op, output):
+        while op:
+            output.append(op)
+            recurse_hierarchy(op.GetDown(), output)
+            op = op.GetNext()
+    all_nodes = []
+    recurse_hierarchy(doc.GetFirstObject(), all_nodes)
+    return all_nodes
 
 def main(doc):
-
-    image_textures_to_check = ArnoldTexture.getPaths(doc)
-    procedurals_to_check = ArnoldProcedural.getPaths(doc)
+    
+    all_objects = getAllObjects(doc)
+    all_assets = getAllCinemaTextures(doc)
+    all_assets.extend(getAllArnoldTextures(doc))
+    all_assets.extend(getAllCinemaProcedurals(doc, all_objects))
+    all_assets.extend(getAllArnoldProcedurals(doc, all_objects))
 
     num_missing = 0
     num_updated = 0
 
-    if len(image_textures_to_check):
-        # Setup pathmapper object
-        pathmapper = PathMapper()
-        if not pathmapper.is_valid:
-            return
+    # Setup pathmapper object
+    pathmapper = PathMapper()
+    if not pathmapper.is_valid:
+        return
 
-        # Update arnold texture image paths
-        (num_img_updated, num_img_missing) = updateImageTexturePaths(
-            image_textures_to_check, pathmapper)
-        num_updated += num_img_updated
-        num_missing += num_img_missing
-
-        # Update arnold procedural paths
-        (num_img_updated, num_img_missing) = updateProceduralPaths(procedurals_to_check, pathmapper)
+    if len(all_assets):
+        (num_img_updated, num_img_missing) = updateFilePaths(all_assets, pathmapper)
         num_updated += num_img_updated
         num_missing += num_img_missing
 
     popup_text = "Checked %s file paths.\nUpdated %s file paths.\nMissing %s file paths.\n\nCheck console for more information." % (
-        len(image_textures_to_check) + len(procedurals_to_check), num_updated, num_missing)
+        len(all_assets), num_updated, num_missing)
 
     c4d.gui.MessageDialog(popup_text)
 
